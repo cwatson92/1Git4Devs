425305d0adc4039b69bf726981e511c80b7dd66c
Connor <connormullett@gmail.com>
2018-09-11 14:30:26 -0400

    Added DataModels

---
+++ CashFlow.Data/NetWorth.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashFlow.Data
+{
+	public class NetWorth
+	{
+		[Key]
+		public int NetWorthId { get; set; }
+
+		public Guid OwnerId { get; set; }
+
+		public decimal SavingsAccount { get; set; }
+
+		public decimal CheckingAccount { get; set; }
+
+		public decimal MoneyMarketAccount { get; set; }
+
+		public decimal SavingsBonds { get; set; }
+
+		public decimal CertificateOfDeposits { get; set; }
+
+		public decimal IRA { get; set; }
+
+		public decimal RothIRA { get; set; }
+
+		public decimal Retirement401K { get; set; }
+
+		public decimal SepIRA { get; set; }
+
+		public decimal Pension { get; set; }
+
+		public decimal Annuity { get; set; }
+
+		public decimal RealEstate { get; set; }
+
+		public decimal PrincipalHome { get; set; }
+
+		public decimal VacationHome { get; set; }
+
+		public decimal CarsTrucksBoats { get; set; }
+
+		public decimal HomeFurnishings { get; set; }
+
+		public decimal OtherAssets { get; set; }
+
+		public decimal TotalAssets
+		{
+			get => SavingsAccount + CheckingAccount + MoneyMarketAccount + SavingsBonds +
+				CertificateOfDeposits + IRA + RothIRA + Retirement401K + SepIRA + Pension +
+				Annuity + RealEstate + PrincipalHome + VacationHome + CarsTrucksBoats +
+				HomeFurnishings + OtherAssets;
+			set { }
+		}
+
+		public decimal CreditCardBalance { get; set; }
+
+		public decimal EstimatedIncome { get; set; }
+
+		public decimal EstimatedIncomeTaxOwed { get; set; }
+
+		public decimal OtherOutstandingBills { get; set; }
+
+		public decimal HomeMortgage { get; set; }
+
+		public decimal HomeEquityLoan { get; set; }
+
+		public decimal MortgagesOnRentals { get; set; }
+
+		public decimal CarLoans { get; set; }
+
+		public decimal StudentLoans { get; set; }
+
+		public decimal LifeInsurancePolicyLoans { get; set; }
+
+		public decimal OtherLongTermDebt { get; set; }
+
+		public decimal TotalLiabilities
+		{
+			get => CreditCardBalance + EstimatedIncome + EstimatedIncomeTaxOwed +
+				OtherOutstandingBills + HomeMortgage + HomeEquityLoan + MortgagesOnRentals +
+				CarLoans + StudentLoans + LifeInsurancePolicyLoans + OtherLongTermDebt;
+			set { }
+		}
+
+		public decimal TotalNetWorth
+		{
+			get => TotalAssets - TotalLiabilities;
+			set { }
+		}
+	}
+}
---
+++ CashFlow.Data/UserInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashFlow.Data
+{
+	public class UserInfo
+	{
+		[Key]
+		public int UserId { get; set; }
+
+		public Guid OwnerId { get; set; }
+
+		public string FirstName { get; set; }
+
+		public string LastName { get; set; }
+
+		public int LastFour { get; set; }
+
+		public string Username { get; set; }
+
+		public Byte[] ProfilePic { get; set; } //TODO: Change this to a dif datatype for persisting images
+	}
+}
