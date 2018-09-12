87cab55dcc8bf343355b89d5a27a0a3f5b152c17
Connor <connormullett@gmail.com>
2018-09-12 13:38:47 -0400

    Working on UserInfo Services

---
+++ CashFlow.Contracts/IUserServices.cs
@@ -0,0 +1,16 @@
+﻿using CashFlow.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashFlow.Contracts
+{
+	public interface IUserServices
+	{
+		bool CreateUser(UserInfo model);
+		bool GetUserInfo(int userId);
+		bool UpdateUserServices(int userId);
+	}
+}
