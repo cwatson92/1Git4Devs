b9df951b3c675cdb2286674085e71d6331b73756
Connor <connormullett@gmail.com>
2018-09-13 09:13:50 -0400

    redid networth controller

--- CashFlow.WebApi/Controllers/NetWorthController.cs
+++ CashFlow.WebApi/Controllers/NetWorthController.cs
@@ -4,73 +4,72 @@ using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
+using System.Web;
 using System.Web.Http;
 
 namespace CashFlow.WebApi.Controllers
 {
-    [Authorize]
-    public class CashflowController : ApiController
-    {
-        public IHttpActionResult Get(int id)
-        {
-            var service = CreateCashFlowService();
-            var netWorth = service.GetNetWorth(id);
-            return Ok(netWorth);
+	[Authorize]
+	public class NetWorthController : ApiController
+	{
+		public IHttpActionResult Get(int id)
+		{
+			var service = CreateCashFlowService();
+			var netWorth = service.GetNetWorth(id);
+			return Ok(netWorth);
 
-        }
+		}
 
-        private NetWorthServices CreateCashFlowService()
-        {
-            var userId = Guid.Parse(User.Identity.GetUserId());
-            return new NetWorthServices(userId);
-        }
+		private NetWorthServices CreateCashFlowService()
+		{
+			var userId = Guid.Parse(User.Identity.GetUserId());
+			return new NetWorthServices(userId);
+		}
 
 
-        public IHttpActionResult Post(NetWorth model)
-        {
-            var service = CreateCashFlowService();
+		public IHttpActionResult Post(NetWorth model)
+		{
+			var service = CreateCashFlowService();
 
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
+			if (!ModelState.IsValid)
+				return BadRequest(ModelState);
 
-            if (!service.CreateNetWorth(model))
-                return InternalServerError();
+			if (!service.CreateNetWorth(model))
+				return InternalServerError();
 
-            return Ok();
-        }
-    
+			return Ok();
+		}
 
-        public IHttpActionResult Put(NetWorth model)
-        {
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
 
-            var service = CreateCashFlowService();
+		public IHttpActionResult Put(NetWorth model)
+		{
+			if (!ModelState.IsValid)
+				return BadRequest(ModelState);
 
-            if (!service.UpdateNetWorth(model))
+			var service = CreateCashFlowService();
 
-                return InternalServerError();
+			if (!service.UpdateNetWorth(model))
 
-            return Ok();
-        }
+				return InternalServerError();
 
-        public IHttpActionResult GetAll()
-        {
-            var networthService = CreateCashFlowService();
-            var networth = networthService.GetNetWorths();
-            return Ok(networth);
-        }
+			return Ok();
+		}
 
-        public IHttpActionResult Delete(int id)
-        {
-            var service = CreateCashFlowService();
+		public IHttpActionResult GetAll()
+		{
+			var networthService = CreateCashFlowService();
+			var networth = networthService.GetNetWorths();
+			return Ok(networth);
+		}
 
-            if (!service.DeleteNetWorth(id))
-                return InternalServerError();
+		public IHttpActionResult Delete(int id)
+		{
+			var service = CreateCashFlowService();
 
-            return Ok();
-        }
-    }
+			if (!service.DeleteNetWorth(id))
+				return InternalServerError();
+
+			return Ok();
+		}
+	}
 }
