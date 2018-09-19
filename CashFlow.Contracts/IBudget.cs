bdc011a502e6429a58b45ad2879f3ddb8ba9cee9
Connor <connormullett@gmail.com>
2018-09-18 21:43:23 -0400

    added services for budget

---
+++ CashFlow.Contracts/IBudget.cs
@@ -0,0 +1,18 @@
+﻿using CashFlow.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashFlow.Contracts
+{
+	public interface IBudget
+	{
+		bool CreateBudget(Budget model);
+		bool DeleteBudget(int id);
+		bool UpdateBudget(Budget model);
+		IEnumerable<Budget> GetBudgets();
+		Budget GetBudget(int id);
+	}
+}
--- CashFlow.Data/Expense.cs
+++ CashFlow.Data/Expense.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,9 @@ namespace CashFlow.Data
 {
 	public class Expense
 	{
+		[Key]
+		public int ExpenseId { get; set; }
+
 		public string Name { get; set; }
 
 		public decimal Cost { get; set; }
