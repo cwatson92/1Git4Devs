2d0a80f16ee8c6b604bd3e692d52a3e9045778cb
Connor <connormullett@gmail.com>
2018-09-20 14:49:02 -0400

    added authorize attributes

--- CashFlow.WebApi/Controllers/BudgetController.cs
+++ CashFlow.WebApi/Controllers/BudgetController.cs
@@ -10,6 +10,7 @@ using System.Web.Http;
 
 namespace CashFlow.WebApi.Controllers
 {
+	[Authorize]
 	public class BudgetController : ApiController
 	{
 		public IHttpActionResult Get(int id)
--- CashFlow.WebApi/Controllers/ExpenseController.cs
+++ CashFlow.WebApi/Controllers/ExpenseController.cs
@@ -10,6 +10,7 @@ using System.Web.Http;
 
 namespace CashFlow.WebApi.Controllers
 {
+	[Authorize]
 	public class ExpenseController : ApiController
 	{
 
